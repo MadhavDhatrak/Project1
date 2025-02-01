3bcb4f7bc8f4ebbd61a4ae5e111c693fca96322d
Ved Mahajan <mahajanved1234@gmail.com>
2025-02-01 12:40:57 +0530

    reverted the / chnages

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
 
 
   const fetchData = useCallback(() => {
-    fetch('/data.json')
+    fetch('data.json')
       .then(response => response.json())
       .then(data => setData(data));
   }, []);
